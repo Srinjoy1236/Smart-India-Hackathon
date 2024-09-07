44547b5e871a55f20e2c0db09031380a8ab62f34
Srinjoy1236 <116112356+Srinjoy1236@users.noreply.github.com>
2024-09-07 09:26:41 +0530

    first commit

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+// import { smallSphere, stars } from "../assets";
+// import Heading from "./Heading";
+// import PricingList from "./PricingList";
+// import { LeftLine, RightLine } from "./design/Pricing";
+
+const Pricing = () => {
+  
+};
+
+export default Pricing;
